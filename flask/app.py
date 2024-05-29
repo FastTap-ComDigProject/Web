44f8e1ad3a92f8487d5afc4d854db70136c6a67e
Danish Alam <41452310+dantyi@users.noreply.github.com>
2024-05-29 00:31:51 -0500

    sipi

--- flask/app.py
+++ flask/app.py
@@ -10,7 +10,7 @@ from werkzeug.utils import secure_filename
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 
 Baudios = 115200
-Puerto = "COM11"
+Puerto = "COM6"
 
 global Serial
 global PorcentajeBaterias
